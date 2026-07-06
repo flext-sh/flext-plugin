5c2db450eb7f1261980748e3553ecba3b627fa43
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 00:21:34 -0300

    fixes

--- src/flext_plugin/_utilities/__init__.py
+++ src/flext_plugin/_utilities/__init__.py
@@ -8,12 +8,8 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_plugin._utilities.discovery import (
-        FlextPluginDiscovery as FlextPluginDiscovery,
-    )
-    from flext_plugin._utilities.plugin_platform import (
-        FlextPluginPlatform as FlextPluginPlatform,
-    )
+    from flext_plugin._utilities.discovery import FlextPluginDiscovery
+    from flext_plugin._utilities.plugin_platform import FlextPluginPlatform
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".discovery": ("FlextPluginDiscovery",),
