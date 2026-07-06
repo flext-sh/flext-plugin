29b9626de46d3a6b41594b7e4c70549c7b8c2784
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:40 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_plugin/__init__.py
+++ src/flext_plugin/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from flext_core.lazy import install_lazy_exports
+from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 from flext_plugin.__version__ import (
     __author__,
     __author_email__,
@@ -16,72 +16,54 @@ from flext_plugin.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_plugin._exports import FLEXT_PLUGIN_LAZY_IMPORTS
 
 if TYPE_CHECKING:
-    from flext_cli import d as d, e as e, h as h, r as r, s as s, x as x
-    from flext_plugin.api import FlextPluginApi as FlextPluginApi, plugin as plugin
-    from flext_plugin.constants import (
-        FlextPluginConstants as FlextPluginConstants,
-        c as c,
-    )
-    from flext_plugin.models import FlextPluginModels as FlextPluginModels, m as m
-    from flext_plugin.protocols import (
-        FlextPluginProtocols as FlextPluginProtocols,
-        p as p,
-    )
-    from flext_plugin.settings import FlextPluginSettings as FlextPluginSettings
-    from flext_plugin.typings import FlextPluginTypes as FlextPluginTypes, t as t
-    from flext_plugin.utilities import (
-        FlextPluginUtilities as FlextPluginUtilities,
-        u as u,
-    )
-
-
-_LAZY_IMPORTS = FLEXT_PLUGIN_LAZY_IMPORTS
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_cli import d, e, h, r, s, x
+    from flext_plugin.api import FlextPluginApi
+    from flext_plugin.constants import FlextPluginConstants, c
+    from flext_plugin.models import FlextPluginModels, m
+    from flext_plugin.protocols import FlextPluginProtocols, p
+    from flext_plugin.settings import FlextPluginSettings
+    from flext_plugin.typings import FlextPluginTypes, t
+    from flext_plugin.utilities import FlextPluginUtilities, u
+_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        ".api": (
+            "FlextPluginApi",
+            "plugin",
+        ),
+        ".constants": (
+            "FlextPluginConstants",
+            "c",
+        ),
+        ".models": (
+            "FlextPluginModels",
+            "m",
+        ),
+        ".protocols": (
+            "FlextPluginProtocols",
+            "p",
+        ),
+        ".settings": ("FlextPluginSettings",),
+        ".typings": (
+            "FlextPluginTypes",
+            "t",
+        ),
+        ".utilities": (
+            "FlextPluginUtilities",
+            "u",
+        ),
+        "flext_cli": (
+            "d",
+            "e",
+            "h",
+            "r",
+            "s",
+            "x",
+        ),
+    },
 )
 
-
-_PUBLIC_EXPORTS: tuple[str, ...] = (
-    "FlextPluginApi",
-    "FlextPluginConstants",
-    "FlextPluginModels",
-    "FlextPluginProtocols",
-    "FlextPluginSettings",
-    "FlextPluginTypes",
-    "FlextPluginUtilities",
-    "plugin",
-    "__author__",
-    "__author_email__",
-    "__description__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "c",
-    "d",
-    "e",
-    "h",
-    "m",
-    "p",
-    "r",
-    "s",
-    "t",
-    "u",
-    "x",
-)
 
 __all__: tuple[str, ...] = (
     "FlextPluginApi",
@@ -118,5 +100,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
