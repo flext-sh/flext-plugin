52233202a79f207d36578e0397db28543ec601f3
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-23 01:20:07 -0300

    fixes

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,8 +9,19 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_cli import cli, core, d, e, h, lazy_attribute, r, services, x
+    from flext_cli import cli
 
+    from flext_core import (
+        core,
+        d,
+        e,
+        h,
+        lazy,
+        lazy_attribute,
+        normalize_lazy_imports,
+        r,
+        x,
+    )
     from flext_plugin import c, config, m, main, p, plugin, s, settings, t, u
 
     from .constants import ExamplesFlextPluginConstants
@@ -33,14 +44,15 @@ __all__: tuple[str, ...] = (
     "d",
     "e",
     "h",
+    "lazy",
     "lazy_attribute",
     "m",
     "main",
+    "normalize_lazy_imports",
     "p",
     "plugin",
     "r",
     "s",
-    "services",
     "settings",
     "t",
     "u",
@@ -55,15 +67,16 @@ _LAZY_IMPORTS = MappingProxyType(
             ".protocols": ("ExamplesFlextPluginProtocols",),
             ".typings": ("ExamplesFlextPluginTypes",),
             ".utilities": ("ExamplesFlextPluginUtilities",),
-            "flext_cli": (
-                "cli",
+            "flext_cli": ("cli",),
+            "flext_core": (
                 "core",
                 "d",
                 "e",
                 "h",
+                "lazy",
                 "lazy_attribute",
+                "normalize_lazy_imports",
                 "r",
-                "services",
                 "x",
             ),
             "flext_plugin": (
