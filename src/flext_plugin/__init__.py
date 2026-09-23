40149fcc82c03e0ef76d47566fdfc5034ce0f134
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-23 14:20:47 -0300

    fixes

--- src/flext_plugin/__init__.py
+++ src/flext_plugin/__init__.py
@@ -20,9 +20,7 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_cli import cli
-
-    from flext_core import core, d, e, h, lazy_attribute, r, x
+    from flext_cli import cli, core, d, e, h, lazy_attribute, r, x
 
     from . import services
     from ._config import FlextPluginConfig, config
@@ -91,8 +89,7 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services": ("services",),
             ".typings": ("FlextPluginTypes", "t"),
             ".utilities": ("FlextPluginUtilities", "u"),
-            "flext_cli": ("cli",),
-            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
+            "flext_cli": ("cli", "core", "d", "e", "h", "lazy_attribute", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
